36ffa217eccfea80f499937b56f0889df378e910
Fabio Henrique Moreira <fa052670@intelbras.com.br>
2020-10-02 18:05:13 -0300

    Primeira Versão

---
+++ consumer.py
@@ -0,0 +1,24 @@
+import paho.mqtt.client as mqtt
+from pymongo import MongoClient
+import json
+import datetime
+
+mongo_client = MongoClient('localhost', 27018)
+mongo_db = mongo_client['ProjetoIN242']
+mongo_collection = mongo_db['contadorpessoas']
+
+
+def msg_recebida(mqtt_client, obj, msg):
+    print('recebendo mensagem...')
+    print(msg.payload)
+    msg_formatada = json.loads(msg.payload)
+    msg_formatada['Dia/hora'] = datetime.datetime.now()
+    mongo_collection.insert_one(msg_formatada)
+    print('mensagem inserida...')
+
+print('Conectando ao broker MQTT...')
+mqtt_client = mqtt.Client()
+mqtt_client.connect('127.0.0.1', 1883)
+mqtt_client.on_message = msg_recebida
+mqtt_client.subscribe('ProjetoIN242')
+mqtt_client.loop_forever()
---
+++ producer.py
@@ -0,0 +1,18 @@
+import time
+import paho.mqtt.client as mqtt
+import random
+import json
+
+print('Conectando ao MQTT Broker...')
+mqtt_client = mqtt.Client()
+mqtt_client.connect('127.0.0.1', 1883)
+
+pessoas = random.randint(0, 10)
+print(pessoas)
+
+mensagem = {
+    'Entrada': 'Porta Principal',
+    'Quantidade': pessoas
+   }
+
+mqtt_client.publish('ProjetoIN242', json.dumps(mensagem))
