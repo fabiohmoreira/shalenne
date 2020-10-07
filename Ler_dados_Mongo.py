daca1c68793fcbb2ed9aae560792c3a627a2ba1c
Fabio Henrique Moreira <fa052670@intelbras.com.br>
2020-10-07 13:08:01 -0300

    Quarta Versão

--- Ler_dados_Mongo.py
+++ Ler_dados_Mongo.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pymongo import MongoClient
+import numpy as np
 
 mongo_client = MongoClient('localhost', 27018)
 mongo_db = mongo_client['ProjetoIN242']
@@ -11,7 +12,9 @@ df = pd.DataFrame.from_records(query)
 
 df_filtro = df[['Entrada', 'Dia/hora', 'Quantidade de pessoas']]  ##seleção de colunas
 
-df_filtro.plot.bar(x='Dia/hora', y='Quantidade de pessoas')
+print (df_filtro['Quantidade de pessoas'].mean())
+
+#df_filtro.plot.bar(x='Dia/hora', y='Quantidade de pessoas')
 
 
 
